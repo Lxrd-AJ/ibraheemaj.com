90c1b09abc5aac22a59d4d134ace2200ae12a582
AJ Ibraheem <ibraheemaj@outlook.com>
2024-11-09 11:54:11 +0000

    Update base

--- endurance/astro.config.mjs
+++ endurance/astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://lxrd-aj.github.io",
-    base: "/ibraheemaj.com"
+    site: "https://lxrd-aj.github.io"
 });
